7ba1c545d69451de65b6cb4c89b5205d3b8dfa8f
alexioannides <alex.ioanndies@yahoo.co.uk>
2024-04-02 08:55:36 +0100

    Add class and test stubs

---
+++ src/llm_toolz/nn.py
@@ -0,0 +1,18 @@
+"""Neural network modules."""
+from torch import nn
+
+
+class MultiHeadAttention(nn.Module):
+    pass
+
+
+class GELU(nn.Module):
+    pass
+
+
+class LayerNorm(nn.Module):
+    pass
+
+
+class TransformerBlock(nn.Module):
+    pass
